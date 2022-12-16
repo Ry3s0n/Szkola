446ed734375e78874728c06c18e6382c6575aa97
Ry3s0n <szkolny.bartosz.grubich@gmail.com>
2022-12-16 13:39:56 +0100

    cos

---
+++ SQL/16.12.2022r/Northwind.sql
@@ -0,0 +1,17 @@
+-- 1. Wyświetl nazwy firm klientów  i daty zamówień
+
+-- 2, Wyświetl dane spedytora z datą zamówienia
+
+-- 3. Wyświetl nazwiska pracowników i daty zamówień
+
+-- 4. Wyświetl nazwy towarów, nazwy kategorii
+
+-- 5. Wyświetl nazwy towarów, nazwy kategorii i idzamówień
+
+-- 6. Wyświetl nazwy towarów, nazwy kategorii, daty zamówień i idklienta
+
+-- 7. Wyświetl nazwy towarów, nazwy kategorii, daty zamówień i nazwę firmy klienta
+
+-- 8. Ile razy był zamawiany dany produkt? (podaj nazwy produktów)
+
+-- 9. Wyświetl nazwy firm klientów, daty zamówień i nazwy towarów
