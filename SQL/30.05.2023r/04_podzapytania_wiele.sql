50731f3464e0ea3fde73f5dd05d0dd90a42717e1
Ry3s0n <szkolny.bartosz.grubich@gmail.com>
2023-05-30 11:31:05 +0200

    Co-authored-by: Ry3s0n <szkolny.bartosz.grubich@gmail.com>

---
+++ SQL/30.05.2023r/04_podzapytania_wiele.sql
@@ -0,0 +1,15 @@
+-- 1.Utwórz zapytanie, które wyświetli ilości produktów dla poszczególnych kategorii
+
+
+
+-- 2. Korzystając z podzapytań wyświetl tylko te wiersze, w których numer kategorii jest większy od 3, a  ilości produktów mieszczą się pomiędzy 6 a 9
+
+
+-- 3. wybieramy te zamówienia, w których łączna ilość zamówionych produktów jest mniejsza od 100
+
+
+-- 4.  Podaj produkty kupowane przez więcej niż 10 klientów (lub 10 różnych klientów - wsk. utwórz zapytanie wybierające różne wiersze CustomerID, ProductID z tabel Orders i OrdersDetalis i użyj jako podzapytanie w części FROM)
+
+
+
+-- 5. Wybierz nazwy i adresy klientów, którzy kupowali więcej niż trzy różne  produkty z kategorii .Confections.
